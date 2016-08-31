e6565cfb69ce55936017b033574daa7a55fdd667
julian <hipogea@hotmail.com>
2016-08-31 00:34:01 -0500

    pruebaspequeñas

---
+++ assets/e4959144/js/flash.js
@@ -0,0 +1,10 @@
+/**
+ * Created by grecia on 14/04/2015.
+ */
+jQuery(function(){
+    $('.link-ocultar').click(function(){
+        $('.cuerpo-mensaje').toggle();
+        return false;
+    });
+
+});
