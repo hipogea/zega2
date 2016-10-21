0bd0ab33db810d1ccd5844157fcbdb9a978f5168
Julian Ramirez <hipogea@hotmail.com>
2016-10-21 07:13:43 -0500

    mejoras en dibujo faleria

---
+++ protected/views/site/otrousuario.php
@@ -0,0 +1,24 @@
+<?php
+$arrayimagenes=$model->recuperaarchivos(false);
+//var_dump($arrayimagenes);
+//$auditoria=$model->getauditoria
+
+     $this->widget('ext.imagegallery1.ImageGallery1',array(
+
+	'images'=>$arrayimagenes,
+
+	'action'=>array('/inventario/borrafotos'),	
+
+	'modelId'=>$modelo->idinventario,		// $model->primarykey (as an example)
+
+ 	'selectedImageId'=>$arrayimagenes[0],	// the ID for your image...any unique ID
+
+	'onSuccess'=>'function(data){  }',
+
+	'onError'=>'function(e){ alert(e);  }',
+
+));                                           
+                                                
+  ?>
+
+
